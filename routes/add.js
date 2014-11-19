4a5fa2f19cb8f850c86d201f4609cc7caee37afc
Christian Bulacan <cbulacan9@gmail.com>
2014-11-19 12:30:00 -0500

    Add page is now working

--- routes/add.js
+++ routes/add.js
@@ -3,10 +3,33 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('add_page');
+  res.render('add_page', {title: 'ADD A PAGE'});
 });
 
 router.post('/submit/', function(req, res) {
+	var models = require('../models/');
+	var title = req.body.title;
+	var body = req.body.content;
+
+	var generateUrlName = function(name) {
+	  if (typeof name != "undefined" && name !== "") {
+	    // Removes all non-alphanumeric characters from name
+	    // And make spaces underscore
+	    return name.replace(/[\s]/ig,"_").replace(/[^\w]/ig,"");
+	  } else {
+	    // Generates random 5 letter string
+	    return Math.random().toString(36).substring(2,7);
+	  }
+	};
+ 
+	var url_name = generateUrlName(title);
+
+	// STUDENT ASSIGNMENT:
+	// add definitions of the 'title', 'body' and 'url_name' variables here
+
+	var p = new models.Page({ "title": title, "body": body, "url_name": url_name });
+	p.save();
+	res.redirect('/');
 });
 
 module.exports = router;
