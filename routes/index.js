4177f4aefd58164effc850f763a0d9ada9feacc7
Christian Bulacan <cbulacan9@gmail.com>
2014-11-19 14:35:11 -0500

    Showing stuff on front page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose')
+var models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'BROWSE MY WIKISTACK' });
+
+	models.Page.find(function(err, pages) {
+		var docs = pages;
+  		res.render('index', { title: 'BROWSE MY WIKISTACK', docs: docs });
+  	});
+
 });
 
 module.exports = router;
